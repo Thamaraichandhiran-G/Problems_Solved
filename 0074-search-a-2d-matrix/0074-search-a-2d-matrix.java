e4ff6f8d0f86b555e38835e781e830fd9db071ec
THAMARAICHANDHIRAN G <thamaraisrikar@gmail.com>
2026-03-23 11:34:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,57 +1,22 @@
 class Solution {
-
-    public boolean search(int[] arr, int t) {
-        int l = 0;
-        int h = arr.length - 1;
-
-        while (l <= h) {
-            int mid = (l + h) / 2;
-
-            if (arr[mid] == t) {
-                return true;
-            } else if (arr[mid] < t) {
-                l = mid + 1;
-            } else {
-                h = mid - 1;
-            }
-        }
-        return false;
-    }
-
-    public int issearch(int[] arr, int t) {
-        int l = 0;
-        int h = arr.length - 1;
-
-        while (l <= h) {
-            int mid = (l + h) / 2;
-
-            if (arr[mid] == t) {
-                return mid;
-            } else if (arr[mid] < t) {
-                l = mid + 1;
-            } else {
-                h = mid - 1;
-            }
-        }
-        return l;
-    }
-
     public boolean searchMatrix(int[][] arr, int t) {
         int n = arr.length;
+        int m = arr[0].length;
 
-        int[] f = new int[n];
-        for (int i = 0; i < n; i++) {
-            f[i] = arr[i][0];
+        int l = 0, h = n * m - 1;
+
+        while (l <= h) {
+            int mid = (l + h) / 2;
+
+            int val = arr[mid / m][mid % m];
+
+            if (val == t)
+                return true;
+            else if (val < t)
+                l = mid + 1;
+            else
+                h = mid - 1;
         }
-
-        int s = issearch(f, t);
-        
-        if (s == n || f[s] > t) {
-            s = s - 1;
-        }
-
-        if (s < 0) return false;
-
-        return search(arr[s], t);
+        return false;
     }
 }
